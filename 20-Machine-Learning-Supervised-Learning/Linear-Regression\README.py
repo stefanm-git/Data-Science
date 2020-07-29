8ffa6d66bd059af584c384507c3b79537576b4fc
stefanm-git <68890615+stefanm-git@users.noreply.github.com>
2020-07-29 12:40:56 +0200

    Create Linear-Regression\README.py

---
+++ 20-Machine-Learning-Supervised-Learning/Linear-Regression\README.py
@@ -0,0 +1,3 @@
+Given a set of points, find a line that fits the data best! Even this simple form of regression allows us to predict future points.
+
+<img src="honey_production.png" alt="img" width="40%" margin-left="100px">
